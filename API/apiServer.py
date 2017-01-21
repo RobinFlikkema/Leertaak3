86835a13326a7a1a6ab0f87e8641f9eec14dfa3e
Jim-David Bennink <jd.bennink@gmail.com>
2017-01-21 17:25:48 +0100

    Update

--- API/apiServer.py
+++ API/apiServer.py
@@ -1,4 +1,4 @@
-from API import measureData, jsonReturn
+from API import measureData
 from bottle import *
 
 # Bottle debug mode
@@ -9,19 +9,19 @@ debug(True)
 @route('/api/station')
 def station_data():
     station = request.query.id
-    date = ("" if request.query.date is "" else request.query.date)  # TODO: set default values
-    time = ("" if request.query.time is "" else request.query.time)  # TODO: set default values
-    limit = (20 if request.query.limit is "" else request.query.limit)
+    date = request.query.date
+    time = request.query.time
+    limit = request.query.limit
     measurements = ['temp', 'wind', 'wind_dir']
-    return measureData.get_stationdata(station, date, time, limit, measurements)
+    return measureData.get_station_data(station, date, time, limit, measurements)
 
 
 @route('/api/country')
 def country_data():
     country = request.query.country
-    limit = (10 if request.query.limit is "" else request.query.limit)
+    limit = request.query.limit
     measurements = ['temp', 'wind', 'wind_dir']
-    return measureData.get_countrydata(country, limit, measurements)
+    return measureData.get_country_data(country, limit, measurements)
 
 
 @error(404)
--- API/measureData.py
+++ API/measureData.py
@@ -1,27 +1,47 @@
 measurements_pos = dict(temp=3, dew=4, air_station=5, air_sea=6, vis=7, wind=8, par=9, snow_fall=10, froze=11, rain=12,
                         snow=13, hail=14, tun=15, tor=16, cloud=17, wind_dir=18)
-measurements_values = {}
+data = {}
 
 
-def get_stationdata(station, date, time, measurements, limit):
+def get_station_data(station, date, time, measurements, limit=20):
+    data['station'] = station
     try:
         with open(date + ".csv", 'r', encoding='utf-8') as csv:
             for line in csv:
-                values = line.strip().split(",")
+                value = line.strip().split(",")
                 if limit:
                     counter = 0
-                    while counter < limit:
-                        for i in (0, measurements - 1):
-                            if values[0] == station and measurements[i] in measurements_pos.keys():
-                                # TODO: add to measurements_values
-                    counter += 1
-                else:
-                    for i in (0, measurements - 1):
-                        if values[0] == station and measurements[i] in measurements_pos.keys():
-                            # TODO: add to measurements_values
+                    while counter < 20:
+                        for i in (0, len(measurements) - 1):
+                            if int(value[0]) == station:
+                                print(station)
+                                if measurements[i] in measurements_pos.keys():
+                                    data[station][value[1]][value[2]].append(value[measurements_pos[i]])
+                        counter += 1
+                elif date:
+                    for i in (0, len(measurements) - 1):
+                        if int(value[0]) == station and value[1] == date:
+                            if measurements[i] in measurements_pos.keys():
+                                data[station][date][value[2]].append(value[measurements_pos[i]])
+                elif time:
+                    for i in (0, len(measurements) - 1):
+                        if int(value[0]) == station and value[2] == time:
+                            if measurements[i] in measurements_pos.keys():
+                                data[station][value[1]][time].append(value[measurements_pos[i]])
+                elif time and date:
+                    for i in (0, len(measurements) - 1):
+                        if int(value[0]) == station and value[1] == date and value[2] == time:
+                            if measurements[i] in measurements_pos.keys():
+                                data[station][date][time].append(value[measurements_pos[i]])
     except IOError:
-        return 'Error', 'Data for the defined date is not available.'
+        # TODO: add error message to data
+        pass
+    return data
 
 
-def get_countrydata(country, limit, measurements):
+def get_country_data(country, limit=10, measurements=None):
     return None
+
+
+if __name__ == '__main__':
+    print(get_station_data(226950, 'weatherdata', time='', measurements=['temp', 'wind', 'wind_dir']))
