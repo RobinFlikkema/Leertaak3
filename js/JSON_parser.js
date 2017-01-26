4acfbe3c2637f75bfd5e6e33cf257e89164cc5e5
Jari <jarikuipers8@gmail.com>
2017-01-26 13:12:04 +0100

    JSON parsing done

---
+++ js/JSON_parser.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    $.ajax({
+        url: 'https://vm.robinflikkema.nl/api/country?name=New+Zealand',
+        type: 'get',
+        dataType: 'json',
+        withCredentials: true,
+
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("Authorization", make_auth());
+        },
+        success: function (data) {
+            var stations = []
+            $.each(data, function (check, station) {                            // Check whether or not there is any data.
+                if (check == "error"){
+                    return;
+                }
+                $.each(station, function (_, station_numbers) {                 // Cycle through stations.
+                    var stationdata = []
+                    $.each(station_numbers, function (_, measurement) {         // Cycle through measurements in the stations.
+                       stationdata.push(measurement);
+                   });
+                    var temp = new Station( stationdata[0],                     // Get all the data and construct a station object.
+                                            stationdata[1],
+                                            stationdata[2],
+                                            stationdata[3]);
+                    stations.push(temp);                                        // Put Station object in an array for further handling.
+                });
+                // TODO: Hier call naar nieuwe functie met stations array.
+            });
+        }
+    });
+});
+
+function make_auth() {
+    var token = 'admin' + ':' + 'test123';
+    var hash = btoa(token);
+    return 'Basic ' + hash;
+}
--- js/Station.js
+++ js/Station.js
@@ -1,10 +1,10 @@
 class Station {
 
-    constructor(id, longitude, latitude){
+    constructor(id, longitude, latitude, measurements){
         this.id_number = id;
         this.longitude = longitude;
         this.latitude = latitude;
-        this.measurements = [];
+        this.measurements = measurements;
     }
 
     addMeasurement(measurement){
@@ -12,6 +12,16 @@ class Station {
     }
 
     printDetails(){
-        console.log(id_number + " : " + longitude + " : " + latitude);
+        console.log("Printing data from station " + this.id_number);
+        console.log("-----------------------------------");
+        console.log(this.longitude + " : " + this.latitude);
+        console.log("-----------------------------------");
+        $.each(this.measurements, function (measurement_number, data) {
+            console.log("MEASUREMENT " + measurement_number);
+            $.each(data, function (value_type, value) {
+                console.log(value_type + " : " + value);
+            });
+        });
+        console.log("");
     }
 }
