4ea3be59ecb34c6d123752cc214fdd749f277f7b
Jim-David Bennink <jd.bennink@gmail.com>
2017-01-30 17:50:13 +0100

    Merge branch 'API' of https://github.com/RobinFlikkema/Leertaak3 into API
    # Conflicts:
    #	API/api_server.py

--- API/api_server.py
+++ API/api_server.py
@@ -11,19 +11,15 @@ debug(False)
 
 class ApiServer:
     """ Control API server and provide API functionality.
-
     This class is being used to control the API server by providing functionality to start the server
     and to provide a way to cleanly shutdown the server.
     This class also provides functions to route request to the appropriate methods defined in other classes.
-
     """
 
     def __init__(self):
         """
-
         Initialize class with Measurement object, define the Bottle app to be used, attach routes to Bottle app
         and create HTTPError object.
-
         """
         self.m = measure_data.Measurements()
         self.app = default_app()
@@ -33,13 +29,10 @@ class ApiServer:
     @staticmethod
     def cleanup():
         """ Remove leftover PID file if present
-
         When the server crashes, the PID file may not have been removed.
         When this is the case, remove the PID file.
-
         Raises:
             FileNotFoundError: the PID file is not present. Pass along when this is the case.
-
         """
         try:
             os.remove("/var/run/api_server.pid.lock")
@@ -48,9 +41,7 @@ class ApiServer:
 
     def routes(self):
         """ Attach routes to Bottle app.
-
         Attach the routes to be used to get measurement data to the defined Bottle app.
-
         """
         self.app.route('/api/station', method="GET", callback=self.station_data)
         self.app.route('/api/country', method="GET", callback=self.country_data)
@@ -58,13 +49,11 @@ class ApiServer:
 
     def start(self, bottle_server='paste', host='localhost', port=8080):
         """
-
         Args:
             bottle_server: the type of server to use
             (see: https://bottlepy.org/docs/dev/deployment.html#switching-the-server-backend).
             host: the IP-address or hostname from the interface to which the Bottle server must be attached to.
             port: the port which the Bottle server must use.
-
         """
 
         # Remove a leftover PID file if present.
@@ -87,13 +76,10 @@ class ApiServer:
 
     def station_data(self):
         """ Retrieve and return measurement data from specific station.
-
         When the /api/station route is being called, the data from the defined station is being returned.
-
         Returns:
             JSON formatted error in case st_id is not defined.
             JSON formatted measurement data in case at least st_id is defined.
-
         """
         st_id = request.query.st_id
         # Get time_from and time_to parameter which is used to retrieve data within a specific time period.
@@ -111,13 +97,11 @@ class ApiServer:
 
     def stations_data(self):
         """ Retrieve and return measurement data from specific stations.
-
         When the /api/stations route is being called, the data from the defined stations is being returned.
-
         Returns:
             JSON formatted error in case station_ids is not defined.
             JSON formatted measurement data in case at least station_ids is defined.
-
+            JSON formatted measurement data in case at least stn_limit is defined.
         """
 
         # A list of station IDs from which measurement data has to be returned.
@@ -125,23 +109,24 @@ class ApiServer:
         time_from = 0 if request.query.time_from is "" else request.query.time_from
         time_to = 0 if request.query.time_to is "" else request.query.time_to
         limit = 20 if request.query.limit is "" else request.query.limit
+        stn_limit = 20 if request.query.limit is "" else request.query.stn_limit
         measurements = ['temp', 'wind', 'wind_dir']
 
-        if station_ids == '':
-            return json_dumps({"error": {"code": "-6", "message": "Station ID missing."}})
+        if station_ids == '' and request.query.limit == '':
+            return json_dumps({"error": {"code": "-6", "message": "Station IDs missing."}})
+        elif request.query.limit == '':
+            return json_dumps({"error": {"code": "-7", "message": "Station limit missing."}})
 
-        return json_dumps(self.m.get_stations_data(station_ids, measurements, int(time_from), int(time_to), int(limit)))
+        return json_dumps(self.m.get_stations_data(measurements, int(time_from), int(time_to), int(limit),
+                                                   int(stn_limit), station_ids))
 
     def country_data(self):
         """ Retrieve and return measurement data based on country name.
-
         When the /api/country route is being called,
         the data from stations present within the defined country is being returned.
-
         Returns:
             JSON formatted error in case name is not defined.
             JSON formatted measurement data in case at least name is defined.
-
         """
         name = request.query.name
         time_from = 0 if request.query.time_from is "" else request.query.time_from
