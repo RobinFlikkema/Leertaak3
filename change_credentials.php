0d98fc7d83f35676aa41cc0b28862cebf4ae1292
Jim-David Bennink <ik.ben.van.een.party@gmail.com>
2017-02-05 16:10:40 +0100

    Update change_credentials.php

--- change_credentials.php
+++ change_credentials.php
@@ -11,14 +11,13 @@
             $this->busyTimeout(1000);
         }
     }
-
     $db = new loginCredentialsDB();
-
     $result = $db->query("SELECT password FROM login WHERE username='{$_SERVER['PHP_AUTH_USER']}'");
     $old_password_from_db = $result->fetchArray();
-
     if ($old_password_from_db[0] == $_SERVER['PHP_AUTH_PW']) {
             $result = $db->exec("UPDATE login SET password='{$_POST['NEW_AUTH_PW']}'");
     }
     $db->close();
-    echo $result;
+    header("Location: /index.html");
+	die();
+?>
