b7f3afca04ca122f315dec29cc8d2ed0fa1095a9
Robin Flikkema <r.flikkema@live.nl>
2017-02-06 15:21:51 +0100

    Added comments to StoreThread

--- src/com/example/leertaak_three/StoreThread.java
+++ src/com/example/leertaak_three/StoreThread.java
@@ -4,17 +4,31 @@ import java.util.ArrayList;
 import java.util.concurrent.BlockingQueue;
 
 /**
- * Created by Robin on 17-12-2016.
+ * Store Thread
+ * This class is used to store Measurements from the storageQueue into a CSV. This file creates a CSV object, which
+ * inserts measurements into a file
+ *
+ * @author Robin Flikkema
+ * @see CSV
  */
 class StoreThread implements Runnable {
     private final CSV CSV;
     private final BlockingQueue<Measurement> storeQueue;
 
+    /**
+     * @param storeQueue is the Queue that this class uses to take Measurements and store them.
+     */
     StoreThread(BlockingQueue<Measurement> storeQueue) {
         this.CSV = new CSV();
         this.storeQueue = storeQueue;
     }
 
+    /**
+     * This is a while loop which checks if there are more than 500 items in the Queue to be stored. We use this check
+     * to make sure that this thread doesn't run for a small amount of items as it involes opening and closing files
+     * which might be very expensive
+     * If there are more then 500 items on the Queue this method will take a maximum of 5.000 items and store them
+     */
     @Override public void run() {
         //noinspection InfiniteLoopStatement
         while (true) {
