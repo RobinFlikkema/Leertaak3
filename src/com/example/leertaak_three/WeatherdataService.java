7b1145b82040bcef1ad3ca93a0c4da1b29abf9ec
Robin Flikkema <r.flikkema@live.nl>
2017-02-06 15:17:01 +0100

    Renamed ParseQueue, Added comments to WeatherdataService

--- src/com/example/leertaak_three/WeatherdataService.java
+++ src/com/example/leertaak_three/WeatherdataService.java
@@ -11,31 +11,33 @@ import java.util.concurrent.atomic.AtomicInteger;
  * WeatherdataService
  * This class is used to spawn the different threads, keep track of queues and accept new connections.
  * The constructor of this class takes a ServerSocket which is used to listen to new connections.
+ *
+ * @author Robin Flikkema
  */
 class WeatherdataService {
     // The port used for receiving weatherdata.
     private static final int SERVER_PORT = 7789;
 
-    private WeatherdataService(ServerSocket serverSocket){
+    private WeatherdataService(ServerSocket serverSocket) {
         // This queue hold Measurements waiting to be processed (checked for missing values etc)
         BlockingQueue<Measurement> checkQueue = new ArrayBlockingQueue<>(100000, true);
         // This queue hold Measurements waiting to be stored
         BlockingQueue<Measurement> storeQueue = new ArrayBlockingQueue<>(100000, true);
         // This queue hold Measurements waiting to be parsed
-        BlockingQueue<ArrayList<String>> incomingQueue = new ArrayBlockingQueue<>(100000, true);
+        BlockingQueue<ArrayList<String>> parseQueue = new ArrayBlockingQueue<>(100000, true);
         // This integer holds the amount of weatherdata that is parsed.
         AtomicInteger parseCounter = new AtomicInteger(0);
 
         // This Station Array is used to hold all Stations. This is later used to calculate missing values.
         Station[] stationList = new Station[1000000];
-        for (int i = 0; i < 1000000; i++){
+        for (int i = 0; i < 1000000; i++) {
             stationList[i] = new Station();
         }
         // Receiver Threads
         ExecutorService receiverThreadPool = Executors.newCachedThreadPool();
         // Parser Threads
         ExecutorService parserThreadPool = Executors.newFixedThreadPool(2);
-        parserThreadPool.submit(new ParserThread(checkQueue, parseCounter, incomingQueue));
+        parserThreadPool.submit(new ParserThread(checkQueue, parseCounter, parseQueue));
         // Check Thread
         ExecutorService checkerThreadPool = Executors.newFixedThreadPool(2);
         checkerThreadPool.submit(new CheckThread(checkQueue, storeQueue, stationList));
@@ -50,7 +52,7 @@ class WeatherdataService {
             try {
                 // This accepts connections and spawns a thread per connection.
                 Socket socket = serverSocket.accept();
-                receiverThreadPool.submit(new ReceiverThread(socket, incomingQueue));
+                receiverThreadPool.submit(new ReceiverThread(socket, parseQueue));
             } catch (IOException e) {
                 // This only happens when an I/O Error occurs while waiting for a connection.
                 e.printStackTrace();
@@ -58,7 +60,7 @@ class WeatherdataService {
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         try {
             System.out.println("Bezig met starten van server op poort " + SERVER_PORT);
             new WeatherdataService(new ServerSocket(SERVER_PORT));
