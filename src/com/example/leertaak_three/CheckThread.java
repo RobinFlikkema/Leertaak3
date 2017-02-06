4ee4ce6be2447db58161e28f4f27201bf988548f
Robin Flikkema <r.flikkema@live.nl>
2017-02-06 15:59:06 +0100

    Added comments to CheckThread

--- src/com/example/leertaak_three/CheckThread.java
+++ src/com/example/leertaak_three/CheckThread.java
@@ -4,19 +4,29 @@ import java.util.ArrayList;
 import java.util.concurrent.BlockingQueue;
 
 /**
- * Created by Robin on 17-12-2016.
+ * This class can be used to create a Thread which checks Measurements and puts them into another Queue
+ *
+ * @author Robin Flikkema
  */
 class CheckThread implements Runnable {
     private final BlockingQueue<Measurement> checkQueue;
     private final BlockingQueue<Measurement> storeQueue;
     private static Station[] stations;
 
+    /**
+     * @param checkQueue , the Queue with Measurements to be Checked
+     * @param storeQueue , the Queue with Measurements that are Checked
+     * @param stations , the Station Lists with the latest Measurements
+     */
     CheckThread(BlockingQueue<Measurement> checkQueue, BlockingQueue<Measurement> storeQueue, Station[] stations) {
         this.checkQueue = checkQueue;
         this.storeQueue = storeQueue;
         CheckThread.stations = stations;
     }
 
+    /**
+     * This functions loops to check for new Measurements to check, then checks them and puts them onto the StoreQueue.
+     */
     @Override public void run() {
         //noinspection InfiniteLoopStatement
         while (true) {
@@ -40,11 +50,18 @@ class CheckThread implements Runnable {
         }
     }
 
+    /**
+     * @param measurement , The Measurement to be checked
+     *
+     * @return , The checked version of the Measurement.
+     */
     private Measurement checkMeasurement(Measurement measurement) {
         int indexOfMissingValue = measurement.valueIsMissing();
         int stationNumber = measurement.getStationNumber();
         double temp = measurement.getTemperature();
 
+        // If there's no missing value the index will be -1
+        // If the missing value is a binary value it will be 11 or larger
         if ( indexOfMissingValue < 11 && indexOfMissingValue > 0) {
             String newValue = String.valueOf(stations[stationNumber].getExtrapolatedValue(indexOfMissingValue));
             measurement.setValue(indexOfMissingValue, newValue);
